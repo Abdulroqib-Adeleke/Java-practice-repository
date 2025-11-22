e618c80448332e16bc9daa5a5aea63e4465ed75a
Abdulroqib Adeleke <162007856+Abdulroqib-Adeleke@users.noreply.github.com>
2025-11-22 07:04:38 +0100

    Using a Scanner class

---
+++ src/Package1/Input.java
@@ -0,0 +1,20 @@
+package Package1;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] arg){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a string: ");
+        String input = scanner.nextLine();
+
+        System.out.println("Enter an integer value:");
+        int val = scanner.nextInt();
+
+        System.out.println("Word entered is: " + input + "\nInteger = " + val);
+
+        scanner.close();
+    }
+}
